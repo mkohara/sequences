832021556818a52713a0caae3be8a9333a80f52c
Maggie O'Hara <mkohara@users.noreply.github.com>
2017-01-29 11:00:15 -0600

    all guesses links to show answer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   get("/all_guesses", {:controller => "sequences", :action =>"all_guesses"})
   get("/show_answer", {:controller => "sequences", :action => "show_answer"})
 end
