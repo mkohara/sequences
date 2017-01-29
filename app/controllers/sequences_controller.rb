22d1d0af7915c05964d41e2d1325385f1fb96c13
Maggie O'Hara <mkohara@users.noreply.github.com>
2017-01-29 11:03:15 -0600

    wrote ruby into the all_guesses
    all_guesses displays what you put into the form — now have to write an
    array for it to say yes or no

--- app/controllers/sequences_controller.rb
+++ app/controllers/sequences_controller.rb
@@ -1,9 +1,13 @@
 class SequencesController < ApplicationController
   def all_guesses
+    @first=params["first_number"]
+    @second=params["second_number"]
+    @third=params["third_number"]
     render("all_guesses.html.erb")
   end
 
   def show_answer
     render("show_answer.html.erb")
   end
+
 end
